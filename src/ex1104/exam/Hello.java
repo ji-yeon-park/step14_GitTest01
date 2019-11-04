8843bdd91bd431c3db150a22671f480a818599b6
kosta <kosta@192.168.0.103>
2019-11-04 17:20:30 +0900

    두번째 수정 상황 TestService.java는 생성 Hello.java는 추가

--- src/ex1104/exam/Hello.java
+++ src/ex1104/exam/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Test 입니다.");
+		System.out.println("추가 된 내용");
 	}
 }
---
+++ src/ex1104/exam/service/TestService.java
@@ -0,0 +1,15 @@
+package ex1104.exam.service;
+
+public class TestService {
+	/**
+	 * test하기
+	 */
+	
+	public void aa() {
+		System.out.println("TestService aa() 추가 ");
+	}
+}
+
+
+
+
