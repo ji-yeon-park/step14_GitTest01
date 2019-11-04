8fddf9daf7dcf5b77bb7b73b4a6838e905a00127
kosta <kosta@192.168.0.103>
2019-11-04 17:23:25 +0900

    세번째 수정

--- src/ex1104/exam/service/TestService.java
+++ src/ex1104/exam/service/TestService.java
@@ -5,8 +5,13 @@ public class TestService {
 	 * test하기
 	 */
 	
-	public void aa() {
+	public void bb() {
 		System.out.println("TestService aa() 추가 ");
+		System.out.println("변경해쓰요");
+	}
+	
+	public void cc() {
+		
 	}
 }
 
